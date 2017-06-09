ac6ed9fbf4798ee74b0e8cb52162d039e55948c6
timcsy <timocsy@yahoo.com.tw>
2017-06-09 15:38:42 +0800

    event finished 2.0

---
+++ GameClient/src/GameServer.java
@@ -0,0 +1,13 @@
+import event.*;
+
+public class GameServer {
+
+	public static void main(String[] args) {
+		EventManager.start();
+	}
+	
+	public static void test() {
+		System.out.println("server te");
+	}
+	
+}
