dd6b6b6e556a41da2ccf9f5c6aeb12bbf1747979
Hori <linmichael40815@gmail.com>
2017-06-10 03:31:53 +0800

    add scene 3 api listener

---
+++ GameClient/src/Character_state.java
@@ -0,0 +1,31 @@
+package packageData;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+
+public class Character_state implements Serializable{
+	
+	private int now_atk;
+	private int now_def;
+	private int hp;
+	private boolean alive;
+	
+	public Character_state(int atk,int def,int hp ,boolean alive)
+	{
+		this.now_atk = atk;
+		this.now_def = def;
+		this.hp = hp;
+		this.alive = alive;
+	}
+	
+	
+	public int get_now_attack(){return this.now_atk;}
+	public int get_now_defence(){return this.now_def;}
+	public int get_hp(){return this.hp;}
+	public boolean get_alive(){return this.alive;}
+	
+	public void set_now_attack(int atk){this.now_atk = atk;}
+	public void set_now_defence(int def){this.now_def = def;}
+	public void set_hp(int hp){ this.hp = hp;}
+	public void set_alive(boolean alive){this.alive = alive;}
+}
