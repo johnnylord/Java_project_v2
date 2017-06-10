cc56a150c4d5e51b26fb0c4a18d38c0d4408d4e9
e652424342007 <sam851214@gmail.com>
2017-06-10 10:52:31 +0800

    temp

--- GameClient/src/GameClient.java
+++ GameClient/src/GameClient.java
@@ -519,13 +519,13 @@ public class GameClient {
 					
 					//when the character are all selected
 					if(myCount==6 && !firstSelect){	
-						EventClient.send("GameClient::my_turn_to_select($...)",new Object[]{comboBox.getSelectedIndex(),oppCount},key);
-						EventClient.send("GameClient::game_is_getting_to_start()",,key);	
+						EventClient.send("GameClient::my_turn_to_select($...)",new Object[]{comboBox.getSelectedIndex(),oppCount},enemyEventClientKey);
+						EventClient.send("GameClient::game_is_getting_to_start()",enemyEventClientKey);	
 						JOptionPane.showMessageDialog(null, "遊戲即將開始");
 						//change to scene 4
 					}
 					else{
-						EventClient.send("GameClient::my_turn_to_select($...)",new Object[]{comboBox.getSelectedIndex(),oppCount},key);
+						EventClient.send("GameClient::my_turn_to_select($...)",new Object[]{comboBox.getSelectedIndex(),oppCount},enemyEventClientKey);
 						oppCount+=2;
 					};
 						
